ac162494e029756ed28f6aa74b72e3731bff524c
Philippe <gaelngando@gmail.com>
2025-07-25 04:51:23 +0100

    Just finished on the side bar

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('teamDropdownBtn').addEventListener('click', () => {
+    document.getElementById('teamDropdown').classList.toggle('hidden');
+});
+
+document.getElementById('userDropdownBtn').addEventListener('click', () => {
+    document.getElementById('userDropdown').classList.toggle('hidden');
+});
